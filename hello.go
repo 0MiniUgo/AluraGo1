75ab6b5aa1680926ce398938ddf997892a5b7f25
0MiniUgo <hugorodrigues969@gmail.com>
2023-09-09 12:45:09 -0300

    Controle de fluxo com if

--- hello.go
+++ hello.go
@@ -11,12 +11,20 @@ func main() {
 	fmt.Println("This pragram is in version", version)
 
 	fmt.Println("1 - Start monitoring")
-	fmt.Println("2 - Show logs")
+	fmt.Println("2 - View logs")
 	fmt.Println("0 - Exit")
 
 	var command int
 	fmt.Scan(&command)
-
 	fmt.Println("The command chosen was", command)
-	fmt.Println(&command)
+
+	if command == 1 {
+		fmt.Println("Monitoring...")
+	} else if command == 2 {
+		fmt.Println("Viewing logs...")
+	} else if command == 0 {
+		fmt.Println("Exiting...")
+	} else {
+		fmt.Println("Invalid code")
+	}
 }
