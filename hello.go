416f381dadd3b173d40ee0c3a4bcd3a57a430ad9
0MiniUgo <hugorodrigues969@gmail.com>
2023-09-09 11:49:37 -0300

    Inferencia de tipos

--- hello.go
+++ hello.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var name string = "Hugo"
-	var version float32 = 1.1
-	fmt.Println("Hello, Mr.", name)
+	name := "Hugo"
+	age := 20
+	version := 1.1
+	fmt.Println("Hello, Mr.", name, "your age is", age)
 	fmt.Println("This pragram is in version", version)
+
+	fmt.Println("TypeOf name: ", reflect.TypeOf(name))
+	fmt.Println("TypeOf age: ", reflect.TypeOf(age))
+	fmt.Println("TypeOf version: ", reflect.TypeOf(version))
 }
